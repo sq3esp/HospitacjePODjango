901417b425e59b0d3560bab360724f9bb5c2e2e8
dawidziobrowski.dz@gmail.com <dawid.ziobrowski@wp.pl>
2023-01-31 23:23:48 +0100

    updated models

--- hospitation_manager/models.py
+++ hospitation_manager/models.py
@@ -26,6 +26,23 @@ class HospitationTeam(models.Model):
     academic_teacher1 = models.ForeignKey(AcademicTeacher, on_delete=models.CASCADE, verbose_name="academic Teacher 1", related_name="academic_teacher1")
     academic_teacher2 = models.ForeignKey(AcademicTeacher, on_delete=models.CASCADE, verbose_name="academic Teacher 2", related_name="academic_teacher2")
 
+class ProtocolAppeal(models.Model):
+    STATUS_CHOICES = [
+        ('OD', 'odrzucone'),
+        ('ZA', 'zatwierdzone'),
+        ('OC', 'oczekuje')]
+    issuer = models.ForeignKey(AcademicTeacher, verbose_name="issuer", on_delete=models.CASCADE)
+    reason = models.TextField(verbose_name="reason")
+    status = models.CharField(verbose_name="status", max_length=2, choices=STATUS_CHOICES, default='OC')
+    dean_response = models.TextField(verbose_name="dean response", blank=True, null=True)
+
+class HospitationProtocol(models.Model):
+    issuer = models.ForeignKey(AcademicTeacher, on_delete=models.CASCADE, verbose_name="issuer")
+    created_at = models.DateField(verbose_name="lodging date", auto_now=False, auto_now_add=True)
+    protocol_content = models.TextField(verbose_name="content of protocol")
+    is_appeal_lodged = models.BooleanField(verbose_name="is_appeal_lodged", default=False)
+    appeal = models.ForeignKey(ProtocolAppeal, verbose_name="appeal", on_delete=models.SET_NULL, blank=True, null=True)
+
 class Hospitation(models.Model):
     STATUS_CHOICES = [
         ('Z', 'Przeprowadzona'),
@@ -38,23 +55,3 @@ class Hospitation(models.Model):
     hospitation_team = models.ForeignKey(HospitationTeam, on_delete=models.SET_NULL, verbose_name="hospitation Team", blank=True, null=True)
     classes = models.ForeignKey(Classes, on_delete=models.CASCADE, verbose_name="classes")
     associated_protocol = models.ForeignKey(HospitationProtocol, on_delete=models.SET_NULL, verbose_name="associated protocol", blank=True, null=True)
-
-class HospitationProtocol(models.Model):
-    hospitation = models.ForeignKey(Hospitation, verbose_name="hospitation", on_delete=models.CASCADE)
-    issuer = models.ForeignKey(AcademicTeacher, on_delete=models.CASCADE, verbose_name="issuer")
-    created_at = models.DateField(verbose_name="lodging date", auto_now=False, auto_now_add=True)
-    protocol_content = models.TextField(verbose_name="content of protocol")
-    is_appeal_lodged = models.BooleanField(verbose_name="is_appeal_lodged" default=False)
-    appeal = models.ForeignKey(ProtocolAppeal, verbose_name="appeal", on_delete=models.SET_NULL, blank=True, null=True)
-
-class ProtocolAppeal(model.Model):
-    STATUS_CHOICES = [
-        ('OD', 'odrzucone'),
-        ('ZA', 'zatwierdzone'),
-        ('OC', 'oczekuje')
-    ]
-
-    issuer = models.ForeignKey(AcademicTeacher, verbose_name="issuer", on_delete=models.CASCADE)
-    reason = models.TextField(verbose_name="reason")
-    status = models.CharField(verbose_name="status", max_length=2, choices=STATUS_CHOICES, default='OC')
-    dean_response = models.TextField(verbose_name="dean response", blank=True, null=True)
