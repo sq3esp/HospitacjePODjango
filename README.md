fbec93ea9e52f790a06878097748cdcbbe4e21ac
dawidziobrowski.dz@gmail.com <dawid.ziobrowski@wp.pl>
2023-02-01 13:24:07 +0100

    updated Readme and settings

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # HospitacjePODjango
  
+## Setup venv and install requirements:
+
+(Windows)
+```
+python -m venv venv
+venv\Scripts\activate
+pip install -r requirements
+```
+
+## Apply migrations
+
+`python manage.py migrate`
+
+## (only on deploy) collect static files
+
+`python manage.py collectstatic`
+
+## Run application
+
+`python manage.py runserver`
--- hospitacje/settings.py
+++ hospitacje/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
+    'hospitation_manager'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
