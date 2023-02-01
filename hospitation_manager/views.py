74bebaa21119f4df750302efa1358dc4ab5c5f8b
dawidziobrowski.dz@gmail.com <dawid.ziobrowski@wp.pl>
2023-02-01 15:09:09 +0100

    added simple index.html

--- hospitation_manager/views.py
+++ hospitation_manager/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'hospitation_manager/layout.html')
+    return render(request, 'hospitation_manager/index.html')
