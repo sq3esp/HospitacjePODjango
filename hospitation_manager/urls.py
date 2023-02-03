44374d7f6c443621d3dc4cce5f963798e3de3034
sq3esp <tomek@plociennik.info>
2023-02-03 17:08:52 +0100

    Create hospitation list and details

--- hospitation_manager/urls.py
+++ hospitation_manager/urls.py
@@ -11,4 +11,11 @@ urlpatterns = [
     path('wzhz/', views.wzhz_index, name = 'wzhz_index'),
     path('wzhz/add', views.wzhz_add, name = 'add_wzhz_member'),
     path('wzhz/details/<int:wzhz_id>', views.wzhz_details),
+
+    path('hospitation_planning/', views.hospitation_planning_index),
+    path('hospitation_planning/details/<int:hospitation_id>', views.hospitation_planning_details),
+    path('hospitation_planning/edit/<int:hospitation_id>', views.hospitation_planning_edit),
+    path('hospitation_planning/add', views.hospitation_planning_add, name = 'add_hospitation'),
 ]
+
+
