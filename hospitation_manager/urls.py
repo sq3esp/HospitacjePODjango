afc4fabf278f28e5f56af2b976012b353800139f
sq3esp <tomek@plociennik.info>
2023-02-03 03:21:58 +0100

    Add working WZHZ managment

--- hospitation_manager/urls.py
+++ hospitation_manager/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('appeal_responses/', views.appeal_responses_index),
     path('appeal_responses/details/<int:id>', views.appeal_responses_details),
 
-    path('wzhz/', views.wzhz_index),
+    path('wzhz/', views.wzhz_index, name = 'wzhz_index'),
     path('wzhz/add', views.wzhz_add, name = 'add_wzhz_member'),
     path('wzhz/details/<int:wzhz_id>', views.wzhz_details),
 ]
--- hospitation_manager/views.py
+++ hospitation_manager/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import ProtocolAppeal, AcademicTeacher
+from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 
 
 def index(request):
@@ -22,21 +24,60 @@ def appeal_responses_details(request, id):
     return render(request, template, context)
 
 
-
+@csrf_exempt 
 def wzhz_index(request):
+    message = ''
+    messageColor = ''
+    if request.method == 'POST':
+        teacher_id = request.POST.get('id')
+        if(teacher_id == None):
+            return HttpResponse('Nie podano id nauczyciela')
+        teacher = get_object_or_404(AcademicTeacher, pk=teacher_id)
+        if(teacher == None):
+            return HttpResponse('Nie znaleziono nauczyciela o podanym id')
+        teacher.belongs_to_WZHZ = False
+        teacher.appointment_to_WZHZ_date = None
+        teacher.save()
+        message = 'Nauczyciel został odwołany z komisji'
+        messageColor = 'green'
+
     template = 'hospitation_manager/wzhz/index.html'
     wzhz_members = AcademicTeacher.objects.filter(belongs_to_WZHZ=True)
-    context = {'wzhz_members': wzhz_members}
+    context = {'wzhz_members': wzhz_members, 'message': message, 'messageColor': messageColor}
     return render(request, template, context)
 
+@csrf_exempt 
 def wzhz_add(request):
+    message = ''
+    messageColor = ''
+    if request.method == 'POST':
+        teacher_id = request.POST.get('id')
+        if(teacher_id == None):
+            return HttpResponse('Nie podano id nauczyciela')
+        teacher = get_object_or_404(AcademicTeacher, pk=teacher_id)
+        if(teacher == None):
+            return HttpResponse('Nie znaleziono nauczyciela o podanym id')
+        teacher.belongs_to_WZHZ = True
+        teacher.appointment_to_WZHZ_date = datetime.today()
+        teacher.save()
+        message = 'Nauczyciel został dodany do WZHZ'
+        messageColor = 'green'
+
+
     template = 'hospitation_manager/wzhz/add.html'
-    context = {}
+    not_wzhz_members = AcademicTeacher.objects.filter(belongs_to_WZHZ=False)
+    context = {'not_wzhz_members': not_wzhz_members, 'message': message, 'messageColor': messageColor}
+
+
     return render(request, template, context)
 
 def wzhz_details(request, wzhz_id):
+    """
+Shows all information about a teacher who is a member of the WZHZ
+    """
     wzhz_member = get_object_or_404(AcademicTeacher, pk=wzhz_id)
     context = {
         'wzhz_member': wzhz_member,
     }
     return render(request, 'hospitation_manager/wzhz/details.html', context)
+
